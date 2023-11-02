8e046a43cacd9f42bf74204d29c0c1a3806cd087
Gabriel Castro <gabrielsilvadecastro777@gmail.com>
2023-11-02 09:57:17 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/gabrielcastro77.png" />
+    <Titulo fontSize={20}>Gabrial Silva</Titulo>
+  </aside>
+)
+
+export default Sidebar
